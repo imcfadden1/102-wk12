40ac3b7662eec80cf3e2ae9061eecff7c71cee99
imcfadden <imcfadden@mymail.mines.edu>
2019-11-16 15:03:13 -0700

    added LoadFile fxn

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,3 +1,12 @@
 def PrintOutput(string1):
     print("OUTPUT", string1)
 PrintOutput("Hello World")
+
+def LoadFile(str1):
+    out=[]
+    with open(str1,'r') as file:
+        for row in file:
+            out.append(row)
+    PrintOutput(out)
+
+LoadFile('Caged.txt')
